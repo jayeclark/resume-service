0c0ef0c66fbdf9f7ee7166b97d165f8f625af816
Jay Clark <jay@jayeclark.dev>
2022-12-07 08:13:34 -0800

    Refactor resume section
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- src/model/Resume/ResumeSection.ts
+++ src/model/Resume/ResumeSection.ts
@@ -1,9 +1,32 @@
 import { Item } from "./Item";
-import { ResumeSectionEntries } from "./ResumeSectionEntry";
+import { Education, Experience, Projects, SkillsAndCertifications } from "./ResumeSectionEntry";
 
-export interface ResumeSection extends Item {
-  type: 'Experience' | 'Education' | 'SkillsAndCertifiations' | 'Projects'
-  content: ResumeSectionEntries
+export type ResumeSection = ExperienceSection | EducationSection | SkillsAndCertificationsSection | ProjectsSection
+
+export interface ExperienceSection extends Item {
+  type: 'Experience';
+  content: Experience;
+  sumOfTotalScores?: number;
+  averageOfTotalScores?: number;
+}
+
+export interface EducationSection extends Item {
+  type: 'Education';
+  content: Education;
+  sumOfTotalScores?: number;
+  averageOfTotalScores?: number;
+}
+
+export interface SkillsAndCertificationsSection extends Item {
+  type: 'Experience';
+  content: SkillsAndCertifications;
+  sumOfTotalScores?: number;
+  averageOfTotalScores?: number;
+}
+
+export interface ProjectsSection extends Item {
+  type: 'Projects';
+  content: Projects;
   sumOfTotalScores?: number;
   averageOfTotalScores?: number;
 }
